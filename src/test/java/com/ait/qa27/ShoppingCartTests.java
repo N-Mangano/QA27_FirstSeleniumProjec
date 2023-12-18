fe139a2db14640bf2f115fe244211f6fd52a8fca
N-Mangano <140650659+N-Mangano@users.noreply.github.com>
2023-12-18 21:31:27 +0100

    add new change

--- src/test/java/com/ait/qa27/ShoppingCartTests.java
+++ src/test/java/com/ait/qa27/ShoppingCartTests.java
@@ -1,6 +1,10 @@
 package com.ait.qa27;
 
+import java.time.Duration;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,6 +15,7 @@ public class ShoppingCartTests extends TestBase{
     if (!isElementPresent(By.cssSelector("[href='/login']"))){
       driver.findElement(By.xpath("//button[.='Log out']")).click();
     }
+
     driver.findElement(By.cssSelector("[href='/login']")).click();
     driver.findElement(By.name("Email")).click();
     driver.findElement(By.name("Email")).clear();
@@ -25,11 +30,41 @@ public class ShoppingCartTests extends TestBase{
   }
 
   @Test
-  public void addItemPositiveTest() {
-  driver.findElement(By.cssSelector(".master-wrapper-page:nth-child(4)")).click();
-  driver.findElement(By.cssSelector(".cart-qty")).click();
+  public void addProductToCartPositiveTest() {
+  driver.findElement(By.cssSelector("[href='/build-your-own-expensive-computer-2']")).click();
+  driver.findElement(By.cssSelector("#add-to-cart-button-74")).click();
+//  driver.findElement(By.cssSelector(".cart-label")).click();
+//  driver.findElement(By.cssSelector(".count")).click();
 
-    Assert.assertTrue(isElementPresent(By.cssSelector(".checkout")));
+//    int sizeBefore = sizeOfItems();
+//    //click on contact
+//    addItems();
+//    pause(1000);
+//    //get size of contacts after remove
+//    int sizeAfter = sizeOfItems();
+//    //assert by number of contacts
+//    Assert.assertEquals(sizeAfter, sizeBefore+1);
+//
+//  }
+//
+//  public void addItems() {
+//  }
+//
+//  public void pause(int millis) {
+//    try {
+//      Thread.sleep(millis);
+//    } catch (InterruptedException e) {
+//      throw new RuntimeException(e);
+//    }
+//  }
+//
+//  public int sizeOfItems() {
+//    if (isElementPresent(By.cssSelector(".cart-qty"))) {
+//      return driver.findElements(By.cssSelector(".cart-qty")).size();
+//    }
+//    return 0;
+
+//    Assert.assertTrue(isElementPresent(By.xpath("//button[@id='checkout']")));
 
   }
 
